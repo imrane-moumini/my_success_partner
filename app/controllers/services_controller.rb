4139a7fcaa3348470232b50f23f4801c323b11e9
Imrane Moumini <imranemouminipro@gmail.com>
2021-06-01 12:36:30 +0200

    add service create and service new + add before_action :authticate_user in the ApplicationContoller

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,25 @@
+class ServicesController < ApplicationController
+
+#GET    /services/new
+  def new
+    @service = Service.new()
+  end
+
+#  POST   /service
+  def create
+    @service = Service.new(service_params)
+   @service.user = current_user
+
+   if @service.save
+    redirect_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+private
+def service_params
+  params.require(:service).permit(:name, :price, :address, :description)
+end
+
+end
