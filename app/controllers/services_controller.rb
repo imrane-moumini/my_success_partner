4b5b1496eb4553a053a520e7cec3872fa18a5782
Imrane Moumini <imranemouminipro@gmail.com>
2021-06-01 12:43:55 +0200

    add indontation

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -8,18 +8,18 @@ class ServicesController < ApplicationController
 #  POST   /service
   def create
     @service = Service.new(service_params)
-   @service.user = current_user
+    @service.user = current_user
 
    if @service.save
     redirect_to service_path(@service)
-    else
-      render :new
-    end
+   else
+    render :new
+   end
   end
 
 private
-def service_params
-  params.require(:service).permit(:name, :price, :address, :description)
-end
+  def service_params
+    params.require(:service).permit(:name, :price, :address, :description)
+  end
 
 end
