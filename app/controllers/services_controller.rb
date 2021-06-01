070564203bf66961bee1654dd65fccddc5b0a0ba
Selma Benmeradi <selma.benmeradi@kedgebs.com>
2021-06-01 17:01:58 +0200

    edit_update

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  
+
   def new
     @service = Service.new()
   end
@@ -15,13 +15,28 @@ class ServicesController < ApplicationController
       render :new
     end
   end
-  
+
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update_attributes(service_params)
+      flash[:success] = "Service updated!"
+      # redirect_to dashboard_path(current_user)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
  def destroy
     @service = Service.find(params[:id])
     @service.destroy
     redirect_to dashboard_path(@dashboard), notice: 'This service was successfully destroyed.'
  end
-  
+
 private
   def service_params
     params.require(:service).permit(:name, :price, :address, :description)
